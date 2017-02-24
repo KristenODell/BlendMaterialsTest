11ada088eb3ce3e2d721d598a921b6bea983b716
Unknown <little_miss_kris07@hotmail.com>
2017-02-24 16:43:13 -0700

    Interface

---
+++ Materials Test/Assets/Scripts/OverloadAnimation1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OverloadAnimation1 : MonoBehaviour
+{
+    public Animator anim;
+    public int i = 0;
+
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+
+	}
+	
+    void OnMouseDown()
+    {
+        i++;
+        switch (i)
+        {
+            case 1:
+                anim.SetBool("Layer1", true);
+                break;
+            case 2:
+                anim.SetBool("Layer2", true);
+                break;
+            case 3:
+                anim.SetBool("Layer3", true);
+                break;
+            case 4:
+                anim.SetBool("Layer4", true);
+                break;
+        }
+    }
+}
