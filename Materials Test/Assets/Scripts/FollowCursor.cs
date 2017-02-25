fe629a123915beb59ff790b953c6e8238252fc38
Unknown <little_miss_kris07@hotmail.com>
2017-02-24 18:23:08 -0700

    HUD part 2

---
+++ Materials Test/Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FollowCursor : MonoBehaviour
+{
+    int i = 5;
+    public GameObject halo;
+    float wait = .2f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //halo = GetComponent<GameObject>();
+        halo.SetActive(false);
+	}
+    
+    public IEnumerator ShootHalo()
+    {
+        while (i > 0)
+        {
+            halo.SetActive(true);
+            yield return new WaitForSeconds(wait);
+            i -= 5;
+        }
+        halo.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+        if(Input.GetMouseButtonDown(0))
+        {
+            i = 5;
+            StartCoroutine(ShootHalo());
+        }
+	}
+
+}
---
+++ Materials Test/Assets/Scripts/SwitchFirstPerson.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchFirstPerson : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
