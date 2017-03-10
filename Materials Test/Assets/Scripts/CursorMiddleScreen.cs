6e65f6aa46f2316e5483ead04116ea77191847b9
Unknown <little_miss_kris07@hotmail.com>
2017-03-09 23:13:56 -0700

    Main Menu

---
+++ Materials Test/Assets/Scripts/CursorMiddleScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorMiddleScreen : MonoBehaviour
+{
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+    }
+}
